6ba21f21b1e7d35a4d80cb5f3ba3d5e93638f180
oller.esteban <estebanoller73@gmail.com>
2024-04-21 19:50:08 -0300

    update in contact

--- src/components/Email.tsx
+++ src/components/Email.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FaLinkedin, FaWhatsapp } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import Link from "next/link";
@@ -7,56 +7,31 @@ import Link from "next/link";
 const EmailSection = () => {
     const [emailSubmitted, setEmailSubmitted] = useState(false);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const data = {
-            email: e.target.email.value,
-            subject: e.target.subject.value,
-            message: e.target.message.value,
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = 'https://widget.tochat.be/bundle.js?key=f2577203-93f3-4dab-bc5b-c1954846cbf6';
+        script.defer = true;
+        script.setAttribute('data-widget-type', 'group'); // Establecer el atributo data-widget-type
+        script.setAttribute('data-person', '6c415cbb-7ef0-4abd-96ef-cea149c3e421'); // Establecer el atributo data-person
+        document.body.appendChild(script);
+
+        // Cleanup
+        return () => {
+            document.body.removeChild(script);
         };
-        const JSONdata = JSON.stringify(data);
-        const endpoint = "/api/send";
-
-        // Form the request for sending data to the server.
-        const options = {
-            // The method is POST because we are sending data.
-            method: "POST",
-            // Tell the server we're sending JSON.
-            headers: {
-                "Content-Type": "application/json",
-            },
-            // Body of the request is the JSON data we created above.
-            body: JSONdata,
-        };
-
-        const response = await fetch(endpoint, options);
-        const resData = await response.json();
-
-        if (response.status === 200) {
-            console.log("Message sent.");
-            setEmailSubmitted(true);
-        }
-    };
+    }, []);
 
     return (
-        <section
-            id="contact"
-            className="section-sec"
-        >
-            <h2 className="text-center text-4xl font-bold text-white">
-                Mis Contactos
-            </h2>
-
-            <div className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative">
-
+        <section id="contact" className="section-sec">
+            <h2 className="text-center text-4xl font-bold text-white">Mis Contactos</h2>
+            <div className="grid md:grid-cols-2 my-12 md:my-12 gap-4 relative">
                 <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
+                <div className="z-10 flex flex-col justify-center">
+                        <div id="whatapp-people-widget"></div>
+                </div>
                 <div className="z-10">
-                    <h5 className="text-xl font-bold text-white my-2">
-                        Conectémonos
-                    </h5>
-                    <p className="text-[#ADB7BE] mb-4 max-w-md">
-                        {" "}
-                        Si quieres ponerte en contacto conmigo, puedes hacerlo a través de las redes sociales o rellenando el formulario de contacto. Te responderé cuanto antes. ¡De verdad que sí! </p>
+                    <h5 className="text-xl font-bold text-white my-2">Conectémonos</h5>
+                    <p className="text-[#ADB7BE] mb-4 max-w-md">Si quieres ponerte en contacto conmigo, puedes hacerlo a través de las redes sociales o rellenando el formulario de contacto. Te responderé cuanto antes. ¡De verdad que sí!</p>
                     <div className="socials flex flex-row gap-2">
                         <Link href="https://www.linkedin.com/in/tu_usuario_de_LinkedIn" target="_blank" rel="noopener noreferrer">
                             <FaLinkedin className="text-white icon" />
@@ -68,52 +43,6 @@ const EmailSection = () => {
                             <MdEmail className="text-white icon" />
                         </Link>
                     </div>
-                </div>
-                <div>
-                    {emailSubmitted ? (
-                        <p className="text-green-500 text-sm mt-2">
-                            ¡Correo electrónico enviado exitosamente!
-                        </p>
-                    ) : (
-                        <form className="flex flex-col" onSubmit={handleSubmit}>
-                            <div className="mb-6">
-                                <label
-                                    htmlFor="email"
-                                    className="text-white block mb-2 text-sm font-medium"
-                                >
-                                    Tu Email
-                                </label>
-                                <input
-                                    name="email"
-                                    type="email"
-                                    id="email"
-                                    required
-                                    className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                                    placeholder="correo@gmail.com"
-                                />
-                            </div>
-                            <div className="mb-6">
-                                <label
-                                    htmlFor="message"
-                                    className="text-white block text-sm mb-2 font-medium"
-                                >
-                                    Mensaje
-                                </label>
-                                <textarea
-                                    name="message"
-                                    id="message"
-                                    className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                                    placeholder="Vamos a hablar acerca de..."
-                                />
-                            </div>
-                            <button
-                                type="submit"
-                                className="bg-button text-white font-medium py-2.5 px-5 rounded-lg w-full"
-                            >
-                                Enviar Mensaje
-                            </button>
-                        </form>
-                    )}
                 </div>
             </div>
         </section>
--- src/components/Main/PortSection.jsx
+++ src/components/Main/PortSection.jsx
@@ -1,16 +1,12 @@
 "use client"
 import { Link } from "react-scroll";
-import { FaReact, FaBackward, FaDatabase, FaRegFileCode, FaDownload, FaGithub, FaDocker } from 'react-icons/fa';
-import { FaWindows } from "react-icons/fa";
 import { FaChevronDown } from 'react-icons/fa';
-import { RiTestTubeFill } from 'react-icons/ri';
-import { SiAstro, SiNestjs } from 'react-icons/si';
-import { FcLinux } from 'react-icons/fc';
 
 const PortSection = () => {
     return (
         <section className="section-port text-white py-20 px-4 md:px-8 md:py-[200px]">
             <div className="container mx-auto">
+                
                 <div className="md:flex md:justify-between">
                     <div className="md:w-1/2 lg:w-2/3">
                         <h2 className="subtitle text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl mb-4">Hola, <span>soy</span></h2>
@@ -23,46 +19,6 @@ const PortSection = () => {
                                     Contáctame
                                 </p>
                             </a>
-                        </div>
-                    </div>
-
-                    <div className="w-full md:w-1/2 lg:w-1/3 mt-8 md:mt-0 md:pl-8 skills md:py-[50px]">
-                        <div className="font-bold">
-                            <div className="text-white items-center">
-                                <p>Mis Habilidades:</p>
-
-                                <p className="flex items-center">
-                                    <FaRegFileCode className="mr-2" />HTML & CSS | JavaScript | TypeScript.
-                                </p>
-                                <p className="flex items-center">
-                                    <FaReact className="mr-2" />React.js | Next.js | Tailwind CSS | SASS.
-                                </p>
-                                <p className="flex items-center">
-                                    <FaBackward className="mr-2" /> Node.js | Express.js | Java.
-                                </p>
-                                <p className="flex items-center">
-                                    <FaDatabase className="mr-2" /> Mongo DB | MySQL.
-                                </p>
-                                <p className="flex items-center">
-                                    <FaGithub className="mr-2" /> GitHub | <FaDocker className="mr-2 ml-2" /> Docker.
-                                </p>
-
-                                <p className="flex items-center">
-                                    <FcLinux className="mr-2" /> Linux. | <FaWindows className="mr-2 ml-2" />Windows
-                                </p>
-                            </div>
-
-                            <div className="text-white pt-5 items-center">
-                                <p>
-                                    Estoy Aprendiendo:
-                                </p>
-                                <p className="flex items-center">
-                                    <RiTestTubeFill className="mr-2" /> Jest | React Testing Library.
-                                </p>
-                                <p className="flex items-center">
-                                    <SiAstro className="mr-2" /> Astro | <SiNestjs className="mr-2" /> Nest.
-                                </p>
-                            </div>
                         </div>
                     </div>
                 </div>
